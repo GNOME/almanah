6b7f9eea63d40f6f46b64c8c629dccc3ec2c7931
Álvaro Peña <alvaropg@gmail.com>
2023-01-13 22:40:46 +0100

    [search-dialog] Removed deprecated g_type_class_add_private

--- src/search-dialog.h
+++ src/search-dialog.h
@@ -1,4 +1,4 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
+/* -*- mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /*
  * Almanah
  * Copyright (C) Philip Withnall 2008 <philip@tecnocode.co.uk>
@@ -20,8 +20,7 @@
 #ifndef ALMANAH_SEARCH_DIALOG_H
 #define ALMANAH_SEARCH_DIALOG_H
 
-#include <glib.h>
-#include <glib-object.h>
+#include <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
@@ -32,11 +31,8 @@ G_BEGIN_DECLS
 #define ALMANAH_IS_SEARCH_DIALOG_CLASS(k)	(G_TYPE_CHECK_CLASS_TYPE ((k), ALMANAH_TYPE_SEARCH_DIALOG))
 #define ALMANAH_SEARCH_DIALOG_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS ((o), ALMANAH_TYPE_SEARCH_DIALOG, AlmanahSearchDialogClass))
 
-typedef struct _AlmanahSearchDialogPrivate	AlmanahSearchDialogPrivate;
-
 typedef struct {
 	GtkDialog parent;
-	AlmanahSearchDialogPrivate *priv;
 } AlmanahSearchDialog;
 
 typedef struct {
