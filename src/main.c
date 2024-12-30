GNOME/almanah

commit 0ba7f05cba7feaf2ae2c220596aead5dfc676675

build: Switch to GtkSourceView 4
No API changes that affect us, other than the need to call init/finalize:

https://gnome.pages.gitlab.gnome.org/gtksourceview/gtksourceview5/porting-guide-3-to-4.html

src/main.c
@@ -33,9 +33,13 @@ main (int argc, char *argv[])
 	g_thread_init (NULL);
 #endif
 
+	gtk_source_init ();
+
 	application = almanah_application_new ();
 	status = g_application_run (G_APPLICATION (application), argc, argv);
 	g_object_unref (application);
+
+	gtk_source_finalize ();
 
 	return status;
 }

meson.build
@@ -31,7 +31,7 @@ gtk = dependency('gtk+-3.0', version: '>= 3.15')
 gmodule = dependency('gmodule-2.0')
 gthread = dependency('gthread-2.0')
 gio = dependency('gio-2.0', version: '>= 2.32')
-gtksourceview = dependency('gtksourceview-3.0')
+gtksourceview = dependency('gtksourceview-4')
 sqlite = dependency('sqlite3')
 cairo = dependency('cairo')
 atk = dependency('atk')

commit 533f30c7e60437cbea7ca5ae901e768922c1a710

Add missing import for gtksourceview
This is needed to call `gtk_source_init()` and `gtk_source_finalize()`.

Fixes: 0ba7f05cba7feaf2ae2c220596aead5dfc676675

src/main.c
@@ -20,6 +20,7 @@
 #include <config.h>
 #include <glib.h>
 #include <gio/gio.h>
+#include <gtksourceview/gtksource.h>
 
 #include "application.h"
 