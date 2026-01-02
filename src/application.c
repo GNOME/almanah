9faa6ce614e1eb58de1240191cbd59282f8e8dbf
Jan Tojnar <jtojnar@gmail.com>
2026-01-02 02:12:47 +0100

    preferences: Make widgets visible by default
    In preparation for replacing `gtk_dialog_run` with `gtk_widget_show`.
    `gtk_widget_show_all` will also be removed in GTK 4:
    https://docs.gtk.org/gtk4/migrating-3to4.html#widgets-are-now-visible-by-default

--- src/application.c
+++ src/application.c
@@ -403,7 +403,6 @@ action_preferences_cb (GSimpleAction *action, GVariant *parameter, gpointer user
 	dialog = almanah_preferences_dialog_new (settings);
 	gtk_window_set_transient_for (GTK_WINDOW (dialog), GTK_WINDOW (priv->main_window));
 
-	gtk_widget_show_all (GTK_WIDGET (dialog));
 	gtk_dialog_run (GTK_DIALOG (dialog));
 
 	gtk_widget_destroy (GTK_WIDGET (dialog));
